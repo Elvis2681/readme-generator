367a683871ed129ce83a10053af20aa84ffc8d4f
Elvis Lee <elvis2681@gmail.com>
2021-04-09 21:14:52 -0700

    read me generator completed

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-// const genMark = require("./utils/generateMarkdown")
 
 inquirer
     .prompt ([
@@ -56,28 +55,34 @@ inquirer
        fs.writeFileSync("testReadme.md",`# ${answer.Title}\n`, (err) => {
         if (err) throw err;
        })
+       const badge = `![badge](https://img.shields.io/static/v1?label=License&message=${answer.License}&color=blue&style=plastic)`;
+       fs.appendFileSync("testReadme.md",`## ${badge}\n`,(err) => {
+        if (err) throw err;
+        })
+        fs.appendFileSync("testReadme.md",`## Table Of Contents\n- [Descripton](#Description)\n- [Installation Instructions](#Installation-Instructions)\n- [Usage Information](#Usage-Information)\n- [Contritbution Guidelines](#Contritbution-Guidelines)\n- [Test Instructions](#Test-Instructions)\n- [Contact](#Contact)\n`,(err) => {
+            if (err) throw err;
+        })
+
        fs.appendFileSync("testReadme.md",`## Description\n${answer.Description}\n`, (err) => {
         if (err) throw err;
        })
        fs.appendFileSync("testReadme.md",`## Installation Instructions\n${answer["Installation Instructions"]}\n`, (err) => {
         if (err) throw err;
        })
-       fs.appendFileSync("testReadme.md",`## ${answer["Usage Information"]}\n`, (err) => {
+       fs.appendFileSync("testReadme.md",`## Usage Information\n${answer["Usage Information"]}\n`, (err) => {
         if (err) throw err;
        })
-       fs.appendFileSync("testReadme.md",`${answer["Contritbution Guidelines"]}\n`, (err) => {
+       fs.appendFileSync("testReadme.md",`## Contritbution Guidelines\n${answer["Contritbution Guidelines"]}\n`, (err) => {
         if (err) throw err;
        })
-       fs.appendFileSync("testReadme.md",`${answer["Test Instructions"]}\n`, (err) => {
+       fs.appendFileSync("testReadme.md",`## Test Instructions\n${answer["Test Instructions"]}\n`, (err) => {
         if (err) throw err;
        })
-       fs.appendFileSync("testReadme.md",`${answer.License}\n`, (err) => {
+       fs.appendFileSync("testReadme.md",`## Contact\n### GitHub Username\n${answer["GitHub Username"]}\n`, (err) => {
         if (err) throw err;
        })
-       fs.appendFileSync("testReadme.md",`${answer["GitHub Username"]}\n`, (err) => {
-        if (err) throw err;
-       })
-       fs.appendFileSync("testReadme.md",`${answer.Email}\n`, (err) => {
+       fs.appendFileSync("testReadme.md",`### Email\n${answer.Email}\n`, (err) => {
         if (err) throw err;
        })
     })
+
--- testReadme.md
+++ testReadme.md
@@ -1,9 +1,24 @@
-# wow
-use it
-MIT
-elvis2681@gmail.com
-elvis
-words
-ua
-use it
-work
+# asd
+## ![badge](https://img.shields.io/static/v1?label=License&message=MIT&color=blue&style=plastic)
+## Table Of Contents
+- [Descripton](#Description)
+- [Installation Instructions](#Installation-Instructions)
+- [Usage Information](#Usage-Information)
+- [Contritbution Guidelines](#Contritbution-Guidelines)
+- [Test Instructions](#Test-Instructions)
+- [Contact](#Contact)
+## Description
+asd
+## Installation Instructions
+asdas
+## Usage Information
+dsad
+## Contritbution Guidelines
+sadsa
+## Test Instructions
+dasd
+## Contact
+### GitHub Username
+asd
+### Email
+asd
